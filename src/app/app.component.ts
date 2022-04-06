348d857ba2b459aae873f625f3cb58936cba80a4
Kim Roger <kim.roger@krogerdev.no>
2022-04-06 09:32:56 +0200

    Leser nå inn json fra assets og viser alle i liste, med bilde på de som har url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SearchViewComponent } from './search-view/search-view.component';
+
 
 @Component({
   selector: 'app-root',
---
+++ src/app/search-view/search-view.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import ArtifactsJson from '../../assets/artifacts.json';
+
+interface ARTIFACT {
+  artifact_id: string;
+  artifact_name: string;
+  created_at: string;
+  created_by_id: string;
+  identifier: string;
+  meta_type: string;
+  object_type: string;
+  superobject_type_id: string;
+  thumbnail_dms_identifier?: string;
+  thumbnail_id: string;
+  updated_at: string;
+  thumbnail_url?: string;
+}
+
+@Component({
+  selector: 'app-search-view',
+  templateUrl: './search-view.component.html',
+  styleUrls: ['./search-view.component.css']
+})
+export class SearchViewComponent implements OnInit {
+
+  Artifacts: ARTIFACT[] = ArtifactsJson;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
