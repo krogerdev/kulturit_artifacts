36aa8f3e2c5ffad966675ba32d5bab9b99ebff5a
Kim Roger <kim.roger@krogerdev.no>
2022-04-06 10:17:18 +0200

    Hovedoppgave fullført, mangler styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SearchViewComponent } from './search-view/search-view.component';
@@ -10,7 +11,8 @@ import { SearchViewComponent } from './search-view/search-view.component';
     SearchViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
